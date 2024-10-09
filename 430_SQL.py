5cc52a436c9d0fa783cc380ea99bb283bf4031ad
abheejani <abheejani@gmail.com>
2024-10-09 17:58:15 -0500

    adding sql query code

---
+++ 430_SQL.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import sqlite3
+from scipy.stats import linregress
+
+# Read the CSV file into a DataFrame
+df = pd.read_csv('C:\\Users\\17323\\Desktop\\statcast.csv', encoding='ISO-8859-1', engine='c', on_bad_lines='skip')
+
+# Connect to the SQLite database (or create it if it doesn't exist)
+conn = sqlite3.connect('statcast.db')
+
+# Save the DataFrame into the SQLite database
+df.to_sql('statcast_data', conn, if_exists='replace', index=False)
+
+# Close the connection to the database
+conn.close()
